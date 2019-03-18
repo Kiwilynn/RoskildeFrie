c1b38f7259cc0e310bee86d7702fc85f25a3a746
Kiwilynn <kasll@hotmail.com>
2019-03-18 11:24:20 +0100

    Påbegyndt opgave

---
+++ src/Menu.java
@@ -0,0 +1,19 @@
+public class Menu {
+
+    public void menu() {
+
+        boolean go = true;
+        int answer = 0;
+
+        while(go){
+
+            System.out.println("1: Vagtplan \n2: Telefon liste\n3: Indskrevet børn");
+        switch (answer) {
+
+            case 1:
+
+        }
+        }
+
+    }
+}
