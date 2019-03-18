dacd5ddc14f232df68b1b1c7a6ce121a73dc866c
MadsRF <madsrunefrederiksen@gmail.com>
2019-03-18 16:03:57 +0100

    added gemBarn and fjernBarn in Bornhaandtering Added text file called børneliste

--- src/Born.java
+++ src/Born.java
@@ -4,13 +4,15 @@ public class Born {
     private String efternavn;
     private int alder;
     private int ind_Dato;
+    private String stue;
     private String parent_Navn;
 
-    public Born(String fornavn, String efternavn, int alder, int ind_Dato, String parent_Navn){
+    public Born(String fornavn, String efternavn, int alder, int ind_Dato, String stue, String parent_Navn){
         this.fornavn = fornavn;
         this.efternavn = efternavn;
         this.alder = alder;
         this. ind_Dato = ind_Dato;
+        this.stue = stue;
         this.parent_Navn = parent_Navn;
     }
 
@@ -26,7 +28,10 @@ public class Born {
     public int getInd_Dato() {
         return ind_Dato;
     }
-    public String parent_Navn() {
+    public String getStue() {
+        return stue;
+    }
+    public String getParent_Navn() {
         return parent_Navn;
     }
 
@@ -41,6 +46,9 @@ public class Born {
     }
     public void setIndDato(int ind_Dato) {
         this.ind_Dato = ind_Dato;
+    }
+    public void setStue(String stue) {
+        this.stue = stue;
     }
     public void setParent_Navn(String parent_Navn) {
         this.parent_Navn = parent_Navn;
--- src/BornHaandtering.java
+++ src/BornHaandtering.java
@@ -1,2 +1,93 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
+
 public class BornHaandtering {
+
+    static List<Born> børneliste = new LinkedList<>();
+    private Scanner input = new Scanner(System.in);
+
+    public void indlæsBørn() throws FileNotFoundException {
+        Scanner input = new Scanner(new File("C:\\Users\\madsr\\IdeaProjects\\untitled1\\src\\Børneliste"));
+        while (input.hasNext()) {
+            String fornavn = input.next();
+            String efternavn = input.next();
+            int alder = input.nextInt();
+            int ind_Dato = input.nextInt();
+            String stue = input.next();
+            String parent_Navn = input.next();
+
+            børneliste.add(new Born(fornavn, efternavn, alder, ind_Dato, stue, parent_Navn));
+        }
+    }
+
+
+    public void gemBarn() throws FileNotFoundException {
+        PrintStream output = new PrintStream(new FileOutputStream("C:\\Users\\madsr\\IdeaProjects\\untitled1\\src\\Børneliste", true));
+        for (Born b : børneliste) {
+            output.println(b.getFornavn()+" "+b.getEfternavn()+" "+b.getAlder()+" "+b.getInd_Dato()+" "+b.getStue()+" "+b.getParent_Navn());
+        }
+        System.out.println();
+    }
+
+    public void fjernBarn()throws FileNotFoundException {
+        System.out.println("Hvilket barn vil du gerne fjerne fra listen?:");
+        System.out.print("Input fornavn: ");
+        String fornavn = input.nextLine().toUpperCase();
+        System.out.print("Input efternavn: ");
+        String efternavn = input.nextLine().toUpperCase();
+
+        for (Born b : børneliste){
+
+            if (b.getFornavn().equals(fornavn) & b.getEfternavn().equals(efternavn)) {
+                System.out.println();
+                børneliste.remove(b);
+                System.out.println("Du slettede: "+b.getFornavn()+" "+b.getEfternavn());
+                System.out.println();
+                gemBarn();
+                break;
+            } else {
+                System.out.println();
+                System.out.println("Dette barn findes ikke ");
+                System.out.println();
+                break;
+            }
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
